63e62d8f046874aef493ebfd605ab1c6c2c13188
reza ul <rezaul.bd77@gmail.com>
2020-10-15 01:02:52 +0600

    added some changes to my repo!

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using ProductMicroServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMicroServices.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProductById(int ProductId);
+
+        void InsertProduct(Product product);
+        void DeleteProduct(int product);
+        void UpdateProduct(Product product);
+        void Save();
+
+
+    }
+}
